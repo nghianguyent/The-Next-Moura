437fabde110a530629a2aab33ac06e69eed5d584
Trung Kien <kien123456k@gmail.com>
2021-06-14 22:09:44 +0700

    resolve config

--- src/App.js
+++ src/App.js
@@ -3,4 +3,5 @@ import Auth from './container/auth'
 function App() {
     return <Auth></Auth>
 }
+
 export default App
--- src/components/Box/index.js
+++ src/components/Box/index.js
@@ -5,6 +5,7 @@ import { StyledBox } from './style'
 export const Box = (props) => {
     return <StyledBox {...props}></StyledBox>
 }
+
 Box.propTypes = {
     padding: PropTypes.string,
     width: PropTypes.string,

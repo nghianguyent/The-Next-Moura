88eb81d2b47f38066d6fee67272e37510c127d69
Trung Kien <kien123456k@gmail.com>
2021-06-14 22:13:06 +0700

    add api caller

--- src/config/index.js
+++ src/config/index.js
@@ -1,9 +1,9 @@
 const publicRuntimeConfig = {
     NODE_ENV: process.env.NODE_ENV || 'production',
-    REACT_APP_API_URL: process.env.REACT_APP_API_URL,
+    API_URL: process.env.REACT_APP_API_URL,
     LOCALSTORAGE_TOKEN_NAME: 'token',
 }
 
-export const { NODE_ENV, REACT_APP_API_URL, LOCALSTORAGE_TOKEN_NAME } = publicRuntimeConfig
+export const { NODE_ENV, API_URL, LOCALSTORAGE_TOKEN_NAME } = publicRuntimeConfig
 
 export default publicRuntimeConfig.NODE_ENV

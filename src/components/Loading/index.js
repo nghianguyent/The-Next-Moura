1767528c4c57cc7a48e94db7d523b572f7da3de4
Hip <cnhh85@gmail.com>
2021-09-11 23:47:32 +0700

    install react-loadingg and use DisappearedLoading to make a Loading component

---
+++ src/components/Loading/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+import { DisappearedLoading } from 'react-loadingg'
+
+const Container = (props) => <DisappearedLoading color={props.color} size={props.size} />
+export default Container

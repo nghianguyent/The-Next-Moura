076782e463135e03ee11c1c41c8cb519dd60b97e
Hip <cnhh85@gmail.com>
2021-09-12 13:59:39 +0700

    fix the loading time to 5000 ticks

--- src/container/home/index.js
+++ src/container/home/index.js
@@ -229,7 +229,7 @@ function Home() {
         setLoading(true)
         setTimeout(() => {
             setLoading(false)
-        }, 0)
+        }, 5000)
     }, [])
     return (
         <PageWrap>
